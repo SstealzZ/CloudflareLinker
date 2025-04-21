3c69d3697a8ce6352be20d42e86ff9cae89c0763
SstealzZ <antoine.veber@epitech.eu>
2025-04-21 10:02:47 +0200

    add __init__.py - Initial creation of __init__.py file for schemas module in FastAPI application

---
+++ fastapi/app/schemas/__init__.py
@@ -0,0 +1,3 @@
+from .user import User, UserCreate, UserUpdate, UserInDB, UserPasswordUpdate, Token, TokenData, FirstTimeSetup
+from .dns_record import DNSRecord, DNSRecordCreate, DNSRecordUpdate, DNSRecordInDB
+from .log import Log, LogCreate, LogInDB 
